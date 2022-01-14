f707148dd55d22520769b8dec2512d26c5a3b352
Samuel Fonseca <samueljosef3@gamil.com>
2022-01-14 09:28:55 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmovie/DSmovieAplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DSmovieAplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoCriadoNaSemanaSpringReactApplication {
+public class DSmovieAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoCriadoNaSemanaSpringReactApplication.class, args);
+		SpringApplication.run(DSmovieAplication.class, args);
 	}
 
 }
