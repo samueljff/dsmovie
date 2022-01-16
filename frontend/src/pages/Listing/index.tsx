7fcd40191ba2d6b1ebd351f1cbcd8fc161c41766
Samuel Fonseca <samueljosef3@gamil.com>
2022-01-16 01:34:58 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,35 +1,42 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState, useEffect} from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utis/requests";
 
 function Listing() {
-    
-    //Forma errada 
-    axios.get(`${BASE_URL}/Movie?size=12&page=2`)
-        .then(Response =>{
-            console.log(Response.data)
-        });
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+            axios.get(`${BASE_URL}/Movie?size=12&page=1`)
+            .then(Response => {
+                const data = Response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number); });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <div className="container">
                 <Pagination />
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <MovieCard />
+                        <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <MovieCard />
+                        <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <MovieCard />
+                        <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <MovieCard />
+                        <MovieCard />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <MovieCard />
+                        <MovieCard />
                     </div>
                 </div>
             </div>
